0894b83e924054957464c93a1a99c5fb774a0731
monkey <opqrst@163.com>
2014-11-18 22:57:44 +0800

    1、添加了弹出层拖拽 2、添加了遮罩 3、添加了设置皮肤功能

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 require.config({
 	paths: {
-		jquery: 'jquery-1.11.0'
+		jquery: 'jquery-1.11.0',
+		jquery_ui: 'jquery-ui-1.10.3.custom'
 	}
 });
 
@@ -9,13 +10,18 @@ require(['jquery', 'window'], function($, w) {
 		new w.Window().alert({
 			width: 500,
 			height: 200,
+			btn4Alert: 'OK Beng!!',
 			hasCloseBtn: true,
+			skinClassName: '',
+			hasMask: true,
+			isDraggable: true,
+			draggableHandle : '.window_header', //设置拖拽目标为标题部分（默认为整个框体）
 			content: "Welcome my alert",
 			fn4AlterBtn: function() {
 				alert('oh fucking , you click me!');
 			},
 			fn4CloseBtn: function() {
-				alert('you close the diaog!');	
+				alert('you close the diaog!');
 			}
 		});
 	});
--- js/window.js
+++ js/window.js
@@ -1,4 +1,4 @@
-define(['jquery'], function($) {
+define(['jquery', 'jquery_ui'], function($, $ui) { //$UI其实没有什么作用，仅仅为了require引入参数能对应上
 	function Window() {
 		this.cfg = {
 			left: 0,
@@ -7,10 +7,13 @@ define(['jquery'], function($) {
 			height: 300,
 			title: '提示',
 			content: '',
+			hasMask: true,
 			btn4Alert: '确定',
 			hasCloseBtn: false,
 			fn4AlterBtn: null,
-			fn4CloseBtn: null
+			fn4CloseBtn: null,
+			skinClassName: '',
+			isDraggable: false
 		};
 	};
 
@@ -18,9 +21,16 @@ define(['jquery'], function($) {
 		alert: function(cfg) {
 			var _cfg = $.extend(this.cfg, cfg);
 			var box = '<div class="window_boundingBox">' + '<div class="window_header">' + _cfg.title + '</div>' + '<div class="window_body">' + _cfg.content + '</div>' + '<div class="window_footer">' + '<input type="button" class="window_alterBtn" value="' + _cfg.btn4Alert + '"/></div>' + '</div>';
-			var $boundingBox = $(box);
-			$boundingBox.appendTo("body");
 
+			//添加遮罩
+			var $mask = null;
+			if (_cfg.hasMask) {
+				$mask = $('<div class="window_mask"></div>');
+				$mask.appendTo("body");
+			}
+
+			$boundingBox = $(box);
+			$boundingBox.appendTo("body");
 			$boundingBox.css({
 				width: _cfg.width + 'px',
 				height: _cfg.height + 'px',
@@ -28,6 +38,23 @@ define(['jquery'], function($) {
 				top: (_cfg.top || (window.innerHeight - _cfg.height) / 2) + 'px'
 			});
 
+			// 设置皮肤
+			if (_cfg.skinClassName) {
+				$boundingBox.addClass(_cfg.skinClassName);
+			}
+
+			// 设置拖拽
+			if (_cfg.isDraggable) {
+				if (_cfg.draggableHandle) { //设置了拖拽目标
+					$boundingBox.draggable({
+						handle: _cfg.draggableHandle
+					});
+				} else { //没有设置为整体可拖拽
+					$boundingBox.draggable();
+				}
+			}
+
+			// 关闭按钮
 			if (_cfg.hasCloseBtn) {
 				var closeBtn = $('<span class="window_closeBtn">X</span>');
 				//把关闭按钮添加到header中（其实添加在最外层的盒子中也行）
@@ -35,14 +62,15 @@ define(['jquery'], function($) {
 				closeBtn.click(function() {
 					_cfg.fn4CloseBtn && _cfg.fn4CloseBtn(); //如果存在回调函数，则处理
 					$boundingBox.remove();
+					$mask && $mask.remove();
 				});
 			}
 
 			var $btn = $boundingBox.find('.window_footer input');
-
 			$btn.click(function() {
 				_cfg.fn4AlterBtn && _cfg.fn4AlterBtn(); //如果存在回调函数，则处理
 				$boundingBox.remove();
+				$mask && $mask.remove();
 			});
 		},
 		confirm: function() {},
